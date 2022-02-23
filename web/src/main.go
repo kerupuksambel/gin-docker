a6d08c32e2985c75ceb844311937888f90ad7875
Ammar Alifian Fahdan <kerupuksambel.2000@gmail.com>
2022-02-23 16:26:31 +0700

    attempt of CRUD

--- web/src/main.go
+++ web/src/main.go
@@ -1,30 +1,78 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
 	"github.com/gin-gonic/gin"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-// Product type
-type Product struct {
-	ID    uint   `json:”id”`
-	Code  string `json:”code”`
-	Price uint   `json:”price”`
+type Note struct {
+	ID    	uint   	`json:"id"`
+	Title  	string 	`json:"title"`
+	Content string  `json:"content"`
 }
 
-func main() {
+func db() *mongo.Client{
+	clientOptions := options.Client().ApplyURI("mongodb://159.203.187.203:27017")
 
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
+
+var noteCollection = db().Database("crud").Collection("notes")
+
+func main() {
 	r := gin.Default()
 
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"greet": "hello, world!",
-		})
+	r.GET("/note", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		var body Note
+		var result primitive.M
+		
+		e := json.NewDecoder(r.Body).Decode(&body)
+		if e != nil{
+			// Kalau tidak ada JSON, tampilkan semua
+			err := noteCollection.Find(context.TODO(), bson.D{}).Decode(&result)
+		}else{
+			if body.ID == nil{
+				fmt.Println("Request invalid")
+			}else{
+				// Kalau ada JSON valid, tampilkan hanya ID
+				err := noteCollection.FindOne(context.TODO(), bson.D{{"id", body.ID}}).Decode(&result)
+			}
+		}
+
+		if err != nil{
+			fmt.Println(err)
+		}
+
+		json.NewEncoder(w).Encode(result)
 	})
 
 	r.GET("/echo/:echo", func(c *gin.Context) {
@@ -49,28 +97,28 @@ func main() {
 		})
 	})
 
-	r.GET("/products", GetProducts)
+	// r.GET("/products", GetProducts)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
-func GetProducts(c *gin.Context) {
+// func GetProducts(c *gin.Context) {
 
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
-	db.AutoMigrate(&Product{})
+// 	db, err := gorm.Open("sqlite3", "test.db")
+// 	if err != nil {
+// 		panic("failed to connect database")
+// 	}
+// 	defer db.Close()
+// 	db.AutoMigrate(&Product{})
 
-	var products []Product
+// 	var products []Product
 
-	if err := db.Find(&products).Error; err != nil {
-		c.AbortWithStatus(http.StatusInternalServerError)
-		log.Println(err)
-	} else {
-		c.JSON(http.StatusOK, products)
-		log.Println(products)
-	}
+// 	if err := db.Find(&products).Error; err != nil {
+// 		c.AbortWithStatus(http.StatusInternalServerError)
+// 		log.Println(err)
+// 	} else {
+// 		c.JSON(http.StatusOK, products)
+// 		log.Println(products)
+// 	}
 
-}
+// }
